f5ff1bc3d97c6cc3f1fb8da6c6ef9b99a24afe29
Bas Volkert <bas.volkert@gmail.com>
2023-06-13 14:34:38 +0200

    new

--- Code.R
+++ Code.R
@@ -1,4 +1,17 @@
-library('xts')
 
-returns <- read.csv('Fama-French-Three-Factors-Model-Data.txt',header=T)
-returns <- xts(returns)
+
+ff_data <- read.table("ff_data.csv",header=TRUE,sep=",")
+
+rmrf <- ff_data[,2]
+smb <- ff_data[,3]
+hml <- ff_data[,4]
+rmw <- ff_data[,5]
+cma <- ff_data[,6]
+rf <- ff_data[,7]
+fund <- ff_data[,8]
+
+fund.xcess <- fund - rf
+
+ffregression <- lm(find.xcess ~ rmrf + smb + hml + rmw + cma)
+
+print(summary(ffregression))
