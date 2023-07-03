29b40eb3616797f92dc652eb0e33c87d74e9efc7
Bas Volkert <bas.volkert@gmail.com>
2023-07-03 11:52:04 +0200

    Results table

--- 5F_MainCode.R
+++ 5F_MainCode.R
@@ -4,11 +4,12 @@ end_date <- "2022-12-30"
 ranges <- data.frame (
   x = c(1,3,5,10,10,10),
   y = c(1,3,5,10,15,20))
+MSFT_test <- getSymbols("MSFT", from = start_date, to = end_date, auto.assign = FALSE)
 
 getting_data <- function(start_date,end_date){
   
   ff_data <- read.table("ff_data.csv",header=TRUE, sep=",")
-  tickers <- c("MSFT", "GOOGL", "VZ", "BIDU")
+  tickers <- c("MSFT", "GOOGL", "VZ", "BIDU","VT")
   returns_table <- data.frame(Date = character(), stringsAsFactors = FALSE)
   
   for (ticker in tickers) { 
@@ -94,7 +95,7 @@ fitting_train_factors_MSFT <- function(train_table, test_table){
   table_name <- deparse(substitute(train_table))
   column_name <-names(train_table)[8]
   print(paste("train data of ",column_name, "of table", table_name))
-  #print(summary(train_regression))
+  print(summary(train_regression))
   a <- predict(train_regression, test_table)
   sum_a <- sum(a)
   b <- sum(test_table$MSFT)
@@ -105,6 +106,61 @@ fitting_train_factors_MSFT <- function(train_table, test_table){
   print(" ")
   return(c)
 } 
+
+
+
+MSFT_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
+  fitting_train_factors_MSFT(train_table1 ,test_table1)
+  fitting_train_factors_MSFT(train_table2, test_table2)
+  fitting_train_factors_MSFT(train_table3, test_table3)
+  fitting_train_factors_MSFT(train_table4, test_table4)
+  fitting_train_factors_MSFT(train_table5, test_table5)
+  fitting_train_factors_MSFT(train_table6, test_table6)
+}
+MSFT_div <- MSFT_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
+
+
+
+
+
+
+
+
+
+
+GOOGL_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
+  fitting_train_factors_GOOGL(train_table1 ,test_table1)
+  fitting_train_factors_GOOGL(train_table2, test_table2)
+  fitting_train_factors_GOOGL(train_table3, test_table3)
+  fitting_train_factors_GOOGL(train_table4, test_table4)
+  fitting_train_factors_GOOGL(train_table5, test_table5)
+  fitting_train_factors_GOOGL(train_table6, test_table6)
+}
+GOOGL_div <- GOOGL_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
+
+VZ_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
+  fitting_train_factors_VZ(train_table1 ,test_table1)
+  fitting_train_factors_VZ(train_table2, test_table2)
+  fitting_train_factors_VZ(train_table3, test_table3)
+  fitting_train_factors_VZ(train_table4, test_table4)
+  fitting_train_factors_VZ(train_table5, test_table5)
+  fitting_train_factors_VZ(train_table6, test_table6)
+}
+VZ_div <- VZ_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
+
+BIDU_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
+  fitting_train_factors_BIDU(train_table1 ,test_table1)
+  fitting_train_factors_BIDU(train_table2, test_table2)
+  fitting_train_factors_BIDU(train_table3, test_table3)
+  fitting_train_factors_BIDU(train_table4, test_table4)
+  fitting_train_factors_BIDU(train_table5, test_table5)
+  fitting_train_factors_BIDU(train_table6, test_table6)
+}
+BIDU_div <- BIDU_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
+
+
+
+
 
 # Function in order to assign the right factors for the Train data set
 fitting_train_factors_GOOGL <- function(train_table, test_table){
@@ -160,43 +216,31 @@ fitting_train_factors_BIDU <- function(train_table, test_table){
   return(c)
 } 
 
-MSFT_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
-  fitting_train_factors_MSFT(train_table1 ,test_table1)
-  fitting_train_factors_MSFT(train_table2, test_table2)
-  fitting_train_factors_MSFT(train_table3, test_table3)
-  fitting_train_factors_MSFT(train_table4, test_table4)
-  fitting_train_factors_MSFT(train_table5, test_table5)
-  fitting_train_factors_MSFT(train_table6, test_table6)
-}
-MSFT_div <- MSFT_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
 
-GOOGL_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
-  fitting_train_factors_GOOGL(train_table1 ,test_table1)
-  fitting_train_factors_GOOGL(train_table2, test_table2)
-  fitting_train_factors_GOOGL(train_table3, test_table3)
-  fitting_train_factors_GOOGL(train_table4, test_table4)
-  fitting_train_factors_GOOGL(train_table5, test_table5)
-  fitting_train_factors_GOOGL(train_table6, test_table6)
-}
-GOOGL_div <- GOOGL_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
 
-VZ_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
-  fitting_train_factors_VZ(train_table1 ,test_table1)
-  fitting_train_factors_VZ(train_table2, test_table2)
-  fitting_train_factors_VZ(train_table3, test_table3)
-  fitting_train_factors_VZ(train_table4, test_table4)
-  fitting_train_factors_VZ(train_table5, test_table5)
-  fitting_train_factors_VZ(train_table6, test_table6)
-}
-VZ_div <- VZ_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
+fitting_train_factors_VT <- function(train_table, test_table){
+  train_regression <- lm(VT - RF ~ Mkt.RF + SMB + HML + RMW + CMA, train_table)
+  table_name <- deparse(substitute(train_table))
+  column_name <-names(train_table)[12]
+  print(paste("train data of ",column_name, "of table", table_name))
+  #print(summary(train_regression))
+  a <- predict(train_regression, test_table)
+  sum_a <- sum(a)
+  b <- sum(test_table$VT)
+  c <- b - sum_a 
+  print(paste("total predicted return of VT:", sum_a))
+  print(paste("total actual return of VT:", b))
+  print(paste("Actual - predicted return of VT:", c))
+  print(" ")
+  return(c)
+} 
 
-BIDU_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
-  fitting_train_factors_BIDU(train_table1 ,test_table1)
-  fitting_train_factors_BIDU(train_table2, test_table2)
-  fitting_train_factors_BIDU(train_table3, test_table3)
-  fitting_train_factors_BIDU(train_table4, test_table4)
-  fitting_train_factors_BIDU(train_table5, test_table5)
-  fitting_train_factors_BIDU(train_table6, test_table6)
+VT_data <-function(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6){
+  fitting_train_factors_VT(train_table1 ,test_table1)
+  fitting_train_factors_VT(train_table2, test_table2)
+  fitting_train_factors_VT(train_table3, test_table3)
+  fitting_train_factors_VT(train_table4, test_table4)
+  fitting_train_factors_VT(train_table5, test_table5)
+  fitting_train_factors_VT(train_table6, test_table6)
 }
-BIDU_div <- BIDU_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
-
+VT_div <- VT_data(train_table1,train_table2,train_table3,train_table4,train_table5,train_table6,test_table1,test_table2,test_table3,test_table4,test_table5,test_table6)
